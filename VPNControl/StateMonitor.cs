ad136ea45e7af612e7c481ce1761f4cc8433a813
osnsergey <osnsergey@gmail.com>
2026-08-02 16:31:03 +0300

    Minor cleanup

--- VPNControl/StateMonitor.cs
+++ VPNControl/StateMonitor.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading;
-using System.Windows.Forms;
+﻿using System.Threading;
 
 namespace VPNControl
 {
--- VPNControl/StatusListener.cs
+++ VPNControl/StatusListener.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace VPNControl
+﻿namespace VPNControl
 {
     interface StatusListener
     {
